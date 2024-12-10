e9c929ae1c5dcc8fdd82f0cf1d1fc8fc1966a43f
noah <noah@gmail.com>
2024-12-10 23:28:49 +0800

    加入line登入功能

--- src/app/Page/external/external.component.ts
+++ src/app/Page/external/external.component.ts
@@ -10,19 +10,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./external.component.css']
 })
 export class ExternalComponent {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
     // 獲取路由中的 path
-    const path = this.route.snapshot.paramMap.get('path');
-    console.log("pathPath", path);
-    if (path) {
-      // 組裝完整的外部 URL
-      const externalUrl = `https:/easy-couple-life.onrender.com/${path}`;
-      window.location.href = externalUrl; // 跳轉到外部路徑
-    } else {
-      // 如果 path 無效，跳回首頁
-      this.router.navigate(['/']);
+    const encodePath = this.route.snapshot.paramMap.get('path');
+    if(encodePath){
+      let path = decodeURIComponent(encodePath);
+
+      if (path) {
+        // 組裝完整的外部 URL
+        const externalUrl = `https://easy-couple-life.onrender.com/${path}`;
+        window.location.href = externalUrl; // 跳轉到外部路徑
+      } else {
+        // 如果 path 無效，跳回首頁
+        this.router.navigate(['/']);
+      }
     }
   }
 }
