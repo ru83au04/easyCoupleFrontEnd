1bd11c4c96c77491812fee82ccc25d7b7df02aaf
ru83au04 <tony151911@gmail.com>
2025-01-08 06:53:00 +0800

    調整首頁計時器格式與排版

--- src/app/Page/home/home.component.ts
+++ src/app/Page/home/home.component.ts
@@ -17,8 +17,8 @@ export class HomeComponent {
   twoReady = false;
   goToAbout = false;
   firstVisit!: boolean;
-  learningTime = { title: "開始學習 Coding累計至今", targetDate: new Date('2023/01/01'), plus: true }
-  careerTime = { title: "從事前端工程師工作累計至今", targetDate: new Date('2024/04/08'), plus: true }
+  learningTime = { title: "學習 Coding累計至今", targetDate: new Date('2023/01/01'), plus: true }
+  careerTime = { title: "從事前端工作累計至今", targetDate: new Date('2024/04/08'), plus: true }
 
   constructor(private router: Router, private active: ActivatedRoute){}
 
