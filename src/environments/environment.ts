973379d4d6d23d9df8796e612b3ec30b30028a57
ru83au04 <tony151911@gmail.com>
2025-03-12 11:43:36 +0800

    忽略 environment.ts 文件

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    googleMapsId: "4c425447edf35d61",
-    googleMapsApiKey: "AIzaSyDgo_AhDHFBAftxhMog2j9YfeIHQ-yzF1U",
-    rootURL: "https://localhost:3000",
-  };
-  
